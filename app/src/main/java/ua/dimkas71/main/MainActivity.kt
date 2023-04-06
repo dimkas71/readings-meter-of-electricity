def182a5e41d8dc207ec2edd6867d528d3e4be04
dimkas71g@gmail.com <dimkas71g@gmail.com>
2023-04-06 08:09:56 +0300

    Wrapped string hello by string resource

--- app/src/main/java/ua/dimkas71/main/MainActivity.kt
+++ app/src/main/java/ua/dimkas71/main/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import ua.dimkas71.ui.theme.ReadingsMetersOfElectricityTheme
 
@@ -31,13 +32,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = stringResource(id = R.string.hello, name))
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ReadingsMetersOfElectricityTheme {
-        Greeting("Android")
+        Greeting("Dmytro")
     }
 }
