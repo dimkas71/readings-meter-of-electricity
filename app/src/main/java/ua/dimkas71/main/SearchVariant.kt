14211767e8dcc82507e21016b50eefe5bd5b7cf6
dimkas71g@gmail.com <dimkas71g@gmail.com>
2023-05-22 13:52:22 +0300

    Moved MeterInfo and ContractInfo to the data package

--- app/src/main/java/ua/dimkas71/main/SearchVariant.kt
+++
@@ -1,9 +0,0 @@
-package ua.dimkas71.main
-
-enum class SearchVariant(val text: String) {
-
-    ByContractNumber("By contract number"),
-    ByNumber("By number"),
-    ByDescription("By description")
-
-}
