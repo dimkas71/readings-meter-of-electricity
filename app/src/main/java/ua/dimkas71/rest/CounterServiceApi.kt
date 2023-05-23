621622d61f9ab4fb67334bf7143d25eb1e070897
dimkas71g@gmail.com <dimkas71g@gmail.com>
2023-05-23 18:43:58 +0300

    Added counter service api

---
+++ app/src/main/java/ua/dimkas71/rest/CounterServiceApi.kt
@@ -0,0 +1,50 @@
+package ua.dimkas71.rest
+
+import android.content.Context
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.Credentials
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Query
+import ua.dimkas71.common.PASSWORD_KEY
+import ua.dimkas71.common.URI_KEY
+import ua.dimkas71.common.USER_KEY
+import ua.dimkas71.common.getSharedPreferences
+import ua.dimkas71.data.SummaryConsumingBySectors
+
+interface CounterServiceApi {
+
+    @GET("consuming")
+    suspend fun getSummaryConsuming(@Query("period") period: String): SummaryConsumingBySectors
+
+    companion object {
+
+        fun service(context: Context): CounterServiceApi {
+            val prefManager = getSharedPreferences(context)
+
+            val baseUri = prefManager.getString(URI_KEY, "")
+            val user = prefManager.getString(USER_KEY, "")
+            val password = prefManager.getString(PASSWORD_KEY, "")
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor {
+                    val credentials = Credentials.basic(user, password)
+                    val requestWithAuth =
+                        it.request().newBuilder().header("Authorization", credentials).build()
+                    it?.proceed(requestWithAuth)!!
+                }.build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl(baseUri)
+                .client(client)
+                .addConverterFactory(Json.asConverterFactory(MediaType.get("application/json")))
+                .build()
+
+            return retrofit.create(CounterServiceApi::class.java)
+        }
+
+    }
+}
