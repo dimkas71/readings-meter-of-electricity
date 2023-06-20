5bd0af18d01c04b0e62ee5e9a03890badd5c09d2
dimkas71g@gmail.com <dimkas71g@gmail.com>
2023-06-20 15:10:30 +0300

    Removed period from SummaryConsumingBySectors model and change type consumed from Double to Long

--- app/src/main/java/ua/dimkas71/data/SummaryConsumingBySectors.kt
+++ app/src/main/java/ua/dimkas71/data/SummaryConsumingBySectors.kt
@@ -1,34 +1,29 @@
 package ua.dimkas71.data
 
-import android.os.Build
-import androidx.annotation.RequiresApi
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 import ua.dimkas71.data.source.local.LocalSummaryConsumingBySectors
 import ua.dimkas71.usecase.DateConverters
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 @Serializable
 data class SummaryConsumingBySectors(
-    @SerialName("period")
-    val period: String,
     @SerialName("sectorNumber")
     val sector: Int,
     @SerialName("consuming")
-    val consumed: Double
+    val consumed: Long
     )
 
-fun SummaryConsumingBySectors.asLocalSummaryBySectors(): LocalSummaryConsumingBySectors {
+fun SummaryConsumingBySectors.asLocalSummaryBySectors(period: String): LocalSummaryConsumingBySectors {
     return LocalSummaryConsumingBySectors(
-        DateConverters.asLong(this.period),
+        DateConverters.asLong(period),
         this.sector,
         this.consumed
     )
 }
+
+fun List<SummaryConsumingBySectors>.asLocal(period: String): List<LocalSummaryConsumingBySectors> =
+    this.map { LocalSummaryConsumingBySectors(
+        DateConverters.asLong(period),
+        it.sector,
+        it.consumed
+    ) }
--- app/src/main/java/ua/dimkas71/data/source/local/LocalSummaryConsumingBySectors.kt
+++ app/src/main/java/ua/dimkas71/data/source/local/LocalSummaryConsumingBySectors.kt
@@ -2,7 +2,8 @@ package ua.dimkas71.data.source.local
 
 import androidx.annotation.NonNull
 import androidx.room.Entity
-import androidx.room.PrimaryKey
+import ua.dimkas71.data.SummaryConsumingBySectors
+import ua.dimkas71.usecase.DateConverters
 
 @Entity(
     tableName = "summary_consuming_by_sectors",
@@ -11,5 +12,13 @@ import androidx.room.PrimaryKey
 data class LocalSummaryConsumingBySectors(
     @NonNull val period: Long,
     @NonNull val sector: Int,
-    @NonNull val consumed: Double
+    @NonNull val consumed: Long
 )
+
+fun List<LocalSummaryConsumingBySectors>.asModel(period: String): List<SummaryConsumingBySectors> =
+    this.filter{ it.period == DateConverters.asLong(period) }.map {
+        SummaryConsumingBySectors(
+            it.sector,
+            it.consumed
+        )
+    }
