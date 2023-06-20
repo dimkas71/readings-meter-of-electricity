92430cc7a57720276820325ee794cd455e20260f
dimkas71g@gmail.com <dimkas71g@gmail.com>
2023-06-20 15:33:13 +0300

    Extracted view model pattern for SummaryConsumingBySectors

--- app/src/main/java/ua/dimkas71/ui/summaryconsuming/SummaryConsumingElectricityBySectorsScreen.kt
+++ app/src/main/java/ua/dimkas71/ui/summaryconsuming/SummaryConsumingElectricityBySectorsScreen.kt
@@ -1,10 +1,19 @@
 package ua.dimkas71.ui.summaryconsuming
 
 import android.util.Log
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -13,45 +22,116 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.viewmodel.compose.viewModel
-import kotlinx.coroutines.flow.collect
+import androidx.compose.ui.unit.dp
+import ua.dimkas71.ui.theme.ReadingsMetersOfElectricityTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun SummaryConsumingElectricityBySectorsScreen(
+    viewModel: SummaryConsumingElectricityBySectorsViewModel,
     onUpClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Summary consuming electricity by sectors") },
-                navigationIcon = {
-                    IconButton(onClick = onUpClick) {
-                        Icon(
-                            Icons.Default.ArrowBack,
-                            contentDescription = null
 
+
+    val datePickerState = rememberDatePickerState(
+        initialDisplayMode = DisplayMode.Input,
+        initialSelectedDateMillis = 0L)
+
+
+
+    ReadingsMetersOfElectricityTheme(
+
+    ) {
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Summary consuming electricity by sectors") },
+                    navigationIcon = {
+                        IconButton(onClick = onUpClick ) {
+                            Icon(
+                                Icons.Default.ArrowBack,
+                                contentDescription = null
+
+                            )
+                        }
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                        navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
+                    )
+
+
+                )
+            }
+
+        ) { contentPadding ->
+            LazyColumn(modifier = Modifier
+                .fillMaxWidth()
+                .padding(contentPadding)) {
+                item {
+                    FlowRow(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        DatePicker(
+                            state = datePickerState,
+                            modifier = Modifier.padding(8.dp),
+                            title = { Text("Report date") },
+                        )
+                        Divider(
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .height(4.dp)
                         )
                     }
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
-                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
-                )
-
-
-            )
-        }
-
-    ) { contentPadding ->
-        Box(modifier = modifier.padding(contentPadding)) {
-            Text("Meters by sectors screen")
-
+                }
+                items(viewModel.uiState.items) {
+                    FlowRow(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text("Sect: ${it.sector}")
+                        Text("${it.consumed} (kWt)", style = MaterialTheme.typography.titleMedium)
+                    }
+                }
+                item {
+                    Divider(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .height(4.dp)
+                    )
+                }
+                item {
+                    FlowRow(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            "Total:",
+                            style = MaterialTheme.typography.titleMedium,
+                            color = MaterialTheme.colorScheme.error
+                        )
+                        Text("${viewModel.uiState.items.sumOf{it.consumed} / 100.0 * 100} (kWt)",
+                            style = MaterialTheme.typography.titleMedium,
+                            color = MaterialTheme.colorScheme.error
+                            )
+                    }
+                }
+            }
+            SideEffect {
+                Log.d("Data changed", datePickerState.selectedDateMillis.toString())
+            }
         }
     }
 }
--- app/src/main/java/ua/dimkas71/ui/summaryconsuming/SummaryConsumingElectricityBySectorsViewModel.kt
+++ app/src/main/java/ua/dimkas71/ui/summaryconsuming/SummaryConsumingElectricityBySectorsViewModel.kt
@@ -1,31 +1,44 @@
 package ua.dimkas71.ui.summaryconsuming
 
-import androidx.lifecycle.SavedStateHandle
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import ua.dimkas71.data.SummaryConsumingBySectors
-import ua.dimkas71.data.SummaryConsumingBySectorsRepository
-
-import ua.dimkas71.common.Result
-import ua.dimkas71.main.R
+import ua.dimkas71.data.repository.SummaryConsumingBySectorsRepository
+import javax.annotation.concurrent.Immutable
 import javax.inject.Inject
 
+@Immutable
 data class SummaryConsumingElectricityBySectorsUiState(
     val items: List<SummaryConsumingBySectors> = emptyList(),
+    val period: String = "01.01.1970",
     val isLoading: Boolean = false,
     val errorMessageId: Int? = null
 )
 
-//@HiltViewModel
-class SummaryConsumingElectricityBySectorsViewModel() {
+@HiltViewModel
+class SummaryConsumingElectricityBySectorsViewModel @Inject constructor(
+    private val repository: SummaryConsumingBySectorsRepository
+): ViewModel() {
 
+    var uiState by mutableStateOf(SummaryConsumingElectricityBySectorsUiState())
+        private set
+
+    private var fetchJob: Job? = null
+
+    fun getSummaryByPeriod(period: String) {
+
+        fetchJob?.cancel()
+
+        fetchJob = viewModelScope.launch {
+            val newItems = repository.getSummaryByPeriod(period)
+            uiState = uiState.copy(items = newItems)
+        }
+    }
 }
