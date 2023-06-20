ae5728123b40d51e04e8b87f5bb46bbf11b08856
dimkas71g@gmail.com <dimkas71g@gmail.com>
2023-06-20 15:12:08 +0300

    Change database version

--- app/src/main/java/ua/dimkas71/data/source/local/AppDatabase.kt
+++ app/src/main/java/ua/dimkas71/data/source/local/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [LocalSummaryConsumingBySectors::class], version = 1, exportSchema = false)
+@Database(entities = [LocalSummaryConsumingBySectors::class], version = 2, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun getLocalSummaryConsumingBySectorsDao(): LocalSummaryConsumingBySectorsDao
 
@@ -18,6 +18,7 @@ abstract class AppDatabase: RoomDatabase() {
                     context,
                     AppDatabase::class.java,
                     "app_database")
+                    .fallbackToDestructiveMigration()
                     .build()
 
                 INSTANCE = instance
