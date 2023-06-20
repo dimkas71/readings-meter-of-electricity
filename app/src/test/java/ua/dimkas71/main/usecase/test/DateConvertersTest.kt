04db510c53f4c48e545b5173b53134923606f9a7
dimkas71g@gmail.com <dimkas71g@gmail.com>
2023-06-20 15:44:00 +0300

    Fixed DateConvertesTest when date format changed

--- app/src/test/java/ua/dimkas71/main/usecase/test/DateConvertersTest.kt
+++ app/src/test/java/ua/dimkas71/main/usecase/test/DateConvertersTest.kt
@@ -8,11 +8,21 @@ class DateConvertersTest {
 
     @Test
     fun firstJanuary1970ZeroShouldBeReturned() {
-        assertEquals(0L, DateConverters.asLong("01.01.1970"))
+        assertEquals(0L, DateConverters.asLong("19700101"))
     }
 
     @Test
     fun firstJanuary2023_1672531200000_ShouldBeReturned() {
-        assertEquals(1672531200000L, DateConverters.asLong("01.01.2023"))
+        assertEquals(1672531200000L, DateConverters.asLong("20230101"))
+    }
+
+    @Test
+    fun asStringTestFirstJanuary1970() {
+        assertEquals("19700101", DateConverters.asString(0L))
+    }
+
+    @Test
+    fun asStringTestJanuary2023() {
+        assertEquals("20230101", DateConverters.asString(1672531200000L))
     }
 }
