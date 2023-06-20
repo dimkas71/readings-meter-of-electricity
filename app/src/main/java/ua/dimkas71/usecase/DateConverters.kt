79a78961c6d936fb6b6df88c842968d21f58d271
dimkas71g@gmail.com <dimkas71g@gmail.com>
2023-06-20 14:06:05 +0300

    Change period converter from a format dd.MM.yyyy to the format: yyyyMMdd

--- app/src/main/java/ua/dimkas71/usecase/DateConverters.kt
+++ app/src/main/java/ua/dimkas71/usecase/DateConverters.kt
@@ -1,6 +1,7 @@
 package ua.dimkas71.usecase
 
 import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.Locale
 import java.util.TimeZone
 
@@ -11,11 +12,18 @@ object DateConverters {
      * @return count of milliseconds from 1970-01-01 - UTC date in milliseconds from 1
      */
     fun asLong(period: String): Long {
-        val formatter = SimpleDateFormat("dd.MM.yyyy").apply {
+        val formatter = SimpleDateFormat("yyyyMMdd").apply {
             timeZone = TimeZone.getTimeZone("UTC")
         }
         return formatter.parse(period).let {
             it.time
         } ?: 0L
+    }
+
+    fun asString(period: Long): String {
+        val formatter = SimpleDateFormat("yyyyMMdd").apply {
+            timeZone = TimeZone.getTimeZone("UTC")
+        }
+        return formatter.format(Date(period))
     }
 }
