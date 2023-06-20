7cd0d72be913c7a90dbd07c1773e2cba6ac2e657
dimkas71g@gmail.com <dimkas71g@gmail.com>
2023-06-20 15:26:00 +0300

    Remove dispatcher  as parameter to the Default value

--- app/src/main/java/ua/dimkas71/data/source/network/SummaryConsumingBySectorsRemoteDataSource.kt
+++ app/src/main/java/ua/dimkas71/data/source/network/SummaryConsumingBySectorsRemoteDataSource.kt
@@ -1,15 +1,14 @@
 package ua.dimkas71.data.source.network
 
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import ua.dimkas71.data.SummaryConsumingBySectors
 import ua.dimkas71.rest.CounterServiceApi
 import javax.inject.Inject
 
-class SummaryConsumingBySectorsRemoteDataSource @Inject constructor(private val api: CounterServiceApi, private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO) {
+class SummaryConsumingBySectorsRemoteDataSource @Inject constructor(private val api: CounterServiceApi) {
     suspend fun getSummaryConsuming(period: String): List<SummaryConsumingBySectors> {
-        val consuming = withContext(ioDispatcher) {
+        val consuming = withContext(Dispatchers.Default) {
             api.getSummaryConsuming(period)
         }
         return consuming
